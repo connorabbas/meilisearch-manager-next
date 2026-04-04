5e2028f98a190677edca99fa9919262a9de056a9
Connor Abbas <abbasconnor@gmail.com>
2026-04-04 22:13:10 +0000

    fix: move app level components out of nuxt layout

--- app/app.vue
+++ app/app.vue
@@ -16,21 +16,21 @@ provide('colorMode', colorMode)
 </script>
 
 <template>
+    <AppToast />
+    <ConfirmDialog
+        group="delete"
+        :draggable="false"
+        blockScroll
+    >
+        <template #icon>
+            <AlertCircle />
+        </template>
+    </ConfirmDialog>
     <NuxtLayout>
         <!-- include tailwind css classes defined in nuxt.config.ts for body tag -->
         <!-- antialiased font-sans h-full bg-surface-100 dark:bg-surface-950 -->
         <NuxtRouteAnnouncer />
         <NuxtLoadingIndicator color="var(--p-primary-500)" />
-        <AppToast />
-        <ConfirmDialog
-            group="delete"
-            :draggable="false"
-            blockScroll
-        >
-            <template #icon>
-                <AlertCircle />
-            </template>
-        </ConfirmDialog>
         <NuxtPage />
     </NuxtLayout>
 </template>

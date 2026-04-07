9f0fd6e73fc1e6b80b68cd983e822435b2014292
Connor Abbas <abbasconnor@gmail.com>
2026-04-07 02:00:42 +0000

    chore: note for TODO

--- app/components/meilisearch/CreateKeyDrawer.vue
+++ app/components/meilisearch/CreateKeyDrawer.vue
@@ -51,6 +51,8 @@ function submitNewKey() {
     })
 }
 
+// TODO: Zod schema validation for submit form
+
 function reset() {
     newKey.value = structuredClone(toRaw(emptyKey))
     allIndexes.value = false
